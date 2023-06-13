65cc650c2a700897a4d24953913e746aaf8dbd04
mkya <dobat00@hotmail.com>
2023-06-13 17:15:25 -0300

    Funcionalidade do botao deletar

--- script.js
+++ script.js
@@ -4,11 +4,13 @@ const input = document.getElementById('input');
 let output = document.getElementById('output');
 let lista = [];
 
+
 teste.addEventListener('click', function(){
     console.log('funcionando')
 })
 
-add.addEventListener('click', render)//{
+
+add.addEventListener('click', render)
 
 input.addEventListener('keydown', function(e){
     if(e.code === "Enter"){
@@ -16,17 +18,20 @@ input.addEventListener('keydown', function(e){
     }
 })
 
-function render(e){
-    if (e.key === 'Enter'){
-        add.click();
-    }
+function render(){
     let element = input.value
     lista.push(element);
     console.log(lista)
     let string = '';
     for (let i = 0; i< lista.length;i++){
-        string += `<li>${lista[i]}</li>`
+        string += `<li>${lista[i]}</li><button class='delete-btn' onclick=deleteTask(${i})>delete</button>`
     }
     output.innerHTML = string;
     input.value =''
 }
+
+
+function deleteTask(index) {
+    lista.splice(index, 1)
+    render();
+  }
